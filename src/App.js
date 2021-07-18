af581bdd307d36b81669065a0438e6f9d8df27de
karkir0003 <karkir0003@gmail.com>
2021-07-18 09:47:23 -0700

    starting up basic React Website

---
+++ src/App.js
@@ -0,0 +1,30 @@
+//import logo from './logo.svg';
+import './App.css';
+//import HelloWorld from './HelloWorld';
+import Home from './Home'
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <HelloWorld />
+        <Home />
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home">
+        <div class="container">
+          <div class="row align-items-center my-5">
+            <div class="col-lg-7">
+              <img
+                class="img-fluid rounded mb-4 mb-lg-0"
+                src="http://placehold.it/900x400"
+                alt=""
+              />
+            </div>
+            <div class="col-lg-5">
+              <h1 class="font-weight-light">Home</h1>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s, when an unknown printer took a galley of
+                type and scrambled it to make a type specimen book.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+// const Home = () => {
+  
+//   function getHome() {
+//     return (
+//       <div className="home">
+//         <div class="container">
+//           <div class="row align-items-center my-5">
+//             <div class="col-lg-7">
+//               <img
+//                 class="img-fluid rounded mb-4 mb-lg-0"
+//                 src="http://placehold.it/900x400"
+//                 alt=""
+//               />
+//             </div>
+//             <div class="col-lg-5">
+//               <h1 class="font-weight-light">Home</h1>
+//               <p>
+//                 Lorem Ipsum is simply dummy text of the printing and typesetting
+//                 industry. Lorem Ipsum has been the industry's standard dummy text
+//                 ever since the 1500s, when an unknown printer took a galley of
+//                 type and scrambled it to make a type specimen book.
+//               </p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+  
+//   return (
+//     getHome
+//   );
+// };
+
+export default Home;
+
