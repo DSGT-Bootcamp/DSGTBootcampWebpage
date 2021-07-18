d79e9ee9a2c80cab1c28a8e9ccdae981e3b79915
karkir0003 <karkir0003@gmail.com>
2021-07-18 09:50:07 -0700

    component directory created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
 //import HelloWorld from './HelloWorld';
-import Home from './Home'
+import Home from './components/Home'
 
 function App() {
   return (
