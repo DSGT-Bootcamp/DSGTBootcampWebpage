f70763f10afc079c8179f2955e53d6dfc3bf83bb
karkir0003 <karkir0003@gmail.com>
2021-08-01 11:55:33 -0400

    test fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders dsgt bootcamp link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/DSGT Bootcamp/i);
   expect(linkElement).toBeInTheDocument();
 });
 // import { render } from '@testing-library/react';
