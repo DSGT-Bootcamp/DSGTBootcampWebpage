087262a55cec8a9b1e271ab06270f20f4543d31b
karkir0003 <karkir0003@gmail.com>
2021-08-01 11:46:32 -0400

    circle ci

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,23 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-import { render } from '@testing-library/react';
-import Enzyme, { mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-// Add your adapter version below
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-Enzyme.configure({ adapter: new Adapter() });
-
-test('displays a quote', () => {
+test('renders learn react link', () => {
   render(<App />);
-  // const quote = document.querySelector('#text p');
-  // expect(quote).toBeInTheDocument();
-  // expect(quote).not.toBeEmptyDOMElement();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
+// import { render } from '@testing-library/react';
+// import Enzyme, { mount } from 'enzyme';
+// import App from './App';
+
+// // Add your adapter version below
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+// Enzyme.configure({ adapter: new Adapter() });
+
+// test('displays a quote', () => {
+//   render(<App />);
+//   // const quote = document.querySelector('#text p');
+//   // expect(quote).toBeInTheDocument();
+//   // expect(quote).not.toBeEmptyDOMElement();
+// });
