a92a1c9597dd6befb2ed6c44ec7af55dbfe2e4ec
karkir0003 <karkir0003@gmail.com>
2021-08-01 11:39:31 -0400

    add circleci config

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+import { render } from '@testing-library/react';
+import Enzyme, { mount } from 'enzyme';
+import App from './App';
+import Quotes from './components/Quotes';
+
+// Add your adapter version below
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test('displays a quote', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const quote = document.querySelector('#text p');
+  expect(quote).toBeInTheDocument();
+  expect(quote).not.toBeEmptyDOMElement();
 });
