69ee7c0d6a4187f609e008bdd32c0209c358175d
karkir0003 <karkir0003@gmail.com>
2021-08-05 09:34:14 -0400

    footer added

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => (
+  <div class="container">
+      <div className="footer">
+        
+        &copy; {new Date().getFullYear()} Copyright: <a href="https://www.datasciencegt.org"> datasciencegt.org </a>
+      </div>
+      
+  </div>
+  
+  
+);
+
+export default Footer;
