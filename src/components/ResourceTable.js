5e3a4bbd45834c08d1813c8f38de4bcfcce7d8af
karkir0003 <54720987+karkir0003@users.noreply.github.com>
2021-09-04 14:08:14 -0400

    ResourceTable

---
+++ src/components/ResourceTable.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class ResourceTable extends React.Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th>Resource Topic/Link</th>
+                    <th>Prerequisite Knowledge</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td> <a href = "https://mml-book.github.io/book/mml-book.pdf">Mathematics for Machine Learning</a></td>
+                    <td>None</td>
+                </tr>
+                <tr>
+                    <td><a href="https://www.youtube.com/watch?v=LHBE6Q9XlzI">Python for Data Science (Beginner)</a></td>
+                    <td>None</td>
+                </tr>
+                <tr>
+                    <td><a href = "https://www.youtube.com/watch?v=HGOBQPFzWKo&list=RDCMUC8butISFwT-Wl7EV0hUK0BQ&index=6">Advanced Python</a></td>
+                    <td>Beginner knowledge of Python</td>
+                </tr>
+                <tr>
+                    <td><a href="https://towardsdatascience.com/machine-learning-basics-part-1-a36d38c7916">Fundamentals of Machine Learning</a></td>
+                    <td>None</td>
+                </tr>
+                <tr>
+                    <td><a href="https://neuralnetworksanddeeplearning.com/index.html">Neural Networks and Deep Learning</a></td>
+                    <td>Familiarity with Linear Algebra, Calculus, and Python</td>
+                </tr>
+                </tbody>
+            </table>
+
+        );
+    }
+}
+export default ResourceTable;
