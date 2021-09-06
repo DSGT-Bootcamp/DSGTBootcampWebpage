94290135db764ff1d08a44682bf9a0b3566b96cd
Harris Barton <69372677+harrisbarton@users.noreply.github.com>
2021-09-05 20:30:01 -0400

    Schedule Changes

--- src/components/ScheduleTable.js
+++ src/components/ScheduleTable.js
@@ -22,36 +22,35 @@ class ScheduleTable extends React.Component {
                     <td><text style={{ color: 'red' }}>September 26th, 2021 11:59pm EDT</text></td>
                   </tr>
                   <tr>
-                    <td><text style={{ color: 'red' }}>2</text></td>
-                      <td><text style={{ color: 'red' }}>September 27th 2021 6pm - 7pm EDT</text></td>
-                      <td><text style={{ color: 'red' }}>Introduction to Pandas + Exploratory Data Analysis</text></td>
-                      <td><text style={{ color: 'red' }}>Stay Tuned!</text></td>
-                      <td><text style={{ color: 'red' }}>October 10th, 2021 11:59pm EDT</text></td>
+                    <td><text style={{ color: 'black' }}>2</text></td>
+                      <td><text style={{ color: 'black' }}>September 27th 2021 6pm - 7pm EDT</text></td>
+                      <td><text style={{ color: 'black' }}>Introduction to Pandas + Exploratory Data Analysis</text></td>
+                      <td><text style={{ color: 'black' }}>Stay Tuned!</text></td>
+                      <td><text style={{ color: 'black' }}>October 10th, 2021 11:59pm EDT</text></td>
                   </tr>
                   <tr>
-                    <td><text style={{ color: 'red' }}>3</text></td>
-                    <td><text style={{ color: 'red' }}>October 11th, 2021 6pm - 7pm EDT</text></td>
-                    <td><text style={{ color: 'red' }}>Introductory Data Preprocessing</text></td>
-                      <td><text style={{ color: 'red' }}>Stay Tuned!</text></td>
-                    <td><text style={{ color: 'red' }}>October 24th, 2021 11:59pm EDT</text></td>
+                    <td><text style={{ color: 'black' }}>3</text></td>
+                    <td><text style={{ color: 'black' }}>October 11th, 2021 6pm - 7pm EDT</text></td>
+                    <td><text style={{ color: 'black' }}>Introductory Data Preprocessing</text></td>
+                      <td><text style={{color: 'black' }}>Stay Tuned!</text></td>
+                    <td><text style={{ color: 'black' }}>October 24th, 2021 11:59pm EDT</text></td>
                   </tr>
                   <tr>
-                      <td><text style={{ color: 'red' }}>4</text></td>
-                      <td><text style={{ color: 'red' }}>October 25th, 2021 6pm - 7pm EDT</text></td>
-                      <td><text style={{ color: 'red' }}>Introduction to Machine Learning</text></td>
-                      <td><text style={{ color: 'red' }}>Stay Tuned!</text></td>
-                      <td><text style={{ color: 'red' }}>November 7th, 2021 11:59pm EDT</text></td>
+                      <td><text style={{ color: 'black' }}>4</text></td>
+                      <td><text style={{ color: 'black' }}>October 25th, 2021 6pm - 7pm EDT</text></td>
+                      <td><text style={{ color: 'black' }}>Introduction to Machine Learning</text></td>
+                      <td><text style={{ color: 'black' }}>Stay Tuned!</text></td>
+                      <td><text style={{ color: 'black' }}>November 7th, 2021 11:59pm EDT</text></td>
                   </tr>
                   <tr>
-                      <td><text style={{ color: 'red' }}>5</text></td>
-                      <td><text style={{ color: 'red' }}>November 8th, 2021 6pm - 7pm EDT</text></td>
-                      <td><text style={{ color: 'red' }}>TBD</text></td>
-                      <td><text style={{ color: 'red' }}>Stay Tuned!</text></td>
-                      <td><text style={{ color: 'red' }}>November 21st, 2021 11:59pm EDT</text></td>
+                      <td><text style={{ color: 'black' }}>5</text></td>
+                      <td><text style={{ color: 'black' }}>November 8th, 2021 6pm - 7pm EDT</text></td>
+                      <td><text style={{ color: 'black' }}>TBD</text></td>
+                      <td><text style={{ color: 'black' }}>Stay Tuned!</text></td>
+                      <td><text style={{ color: 'black' }}>November 21st, 2021 11:59pm EDT</text></td>
                   </tr>
               </tbody>
             </table>
-
         );
     }
 }
