308435ff1a008251d3c6478b0dda2edfca82d065
Harris Barton <69372677+harrisbarton@users.noreply.github.com>
2021-10-07 10:06:15 -0400

    Put drill links into the schedule.

--- src/components/ScheduleTable.js
+++ src/components/ScheduleTable.js
@@ -9,7 +9,7 @@ class ScheduleTable extends React.Component {
                     <th>Workshop #</th>
                     <th>Date + Location</th>
                     <th>Topic</th>
-                    <th>Link to Workshop Notebook + Recording</th>
+                    <th>Link to Workshop Notebook + Recording + Drill</th>
                     <th>Milestone Deadline + Link to Deliverable</th>
                   </tr>
               </thead>
@@ -18,14 +18,16 @@ class ScheduleTable extends React.Component {
                     <td><text style={{ color: 'red' }}>1</text></td>
                     <td><text style={{ color: 'red' }}>September 13th 2021 6:30 pm - 7:30 pm EDT @ COC 017</text></td>
                     <td><text style={{ color: 'red' }}>Introduction to Python + Intro to Bootcamp Project</text></td>
-                    <td><a href = "https://colab.research.google.com/drive/1KGBoGInffse24QAsq-tkeomokL1tfgdS"><text style={{ color: 'red' }}>Colab Notebook</text></a><a href = " https://drive.google.com/file/d/1ELdCTK0sXlFpAITf8TqSfLpC-jl9v8cv/view"><text style={{ color: 'red' }}>, Recording</text></a></td>
+                    <td><a href = "https://colab.research.google.com/drive/1KGBoGInffse24QAsq-tkeomokL1tfgdS"><text style={{ color: 'red' }}>Colab Notebook</text></a><a href = " https://drive.google.com/file/d/1ELdCTK0sXlFpAITf8TqSfLpC-jl9v8cv/view"><text style={{ color: 'red' }}>, Recording</text></a>
+                    <a href = "https://colab.research.google.com/drive/1ZSALyFdGv0izBhUc53hlHmC757PqWxCI"><text style={{ color: 'red' }}>, Drill</text></a></td>
                     <td><text style={{ color: 'red' }}>September 26th, 2021 11:59pm EDT</text></td>
                   </tr>
                   <tr>
                     <td><text style={{ color: 'red' }}>2</text></td>
                       <td><text style={{ color: 'red' }}>September 27th 2021 6pm - 7pm EDT</text></td>
                       <td><text style={{ color: 'red' }}>Introduction to Pandas + Exploratory Data Analysis</text></td>
-                      <td><a href = "https://colab.research.google.com/drive/1EJwP03qsA2nFroKm8w2e3ddNrMYhI0qV"><text style={{ color: 'red' }}>Colab Notebook</text></a><a href = " https://drive.google.com/file/d/1tAmCUWcy_MwSPHiIgCffCdHCbEwGLvnF/view?usp=sharing"><text style={{ color: 'red' }}>, Recording</text></a></td>
+                      <td><a href = "https://colab.research.google.com/drive/1EJwP03qsA2nFroKm8w2e3ddNrMYhI0qV"><text style={{ color: 'red' }}>Colab Notebook</text></a><a href = " https://drive.google.com/file/d/1tAmCUWcy_MwSPHiIgCffCdHCbEwGLvnF/view?usp=sharing"><text style={{ color: 'red' }}>, Recording</text></a>
+                      <a href = "https://colab.research.google.com/drive/17hH8jnZ4T4VvFBQfQXfLnF7pvLguAXcP"><text style={{ color: 'red' }}>, Drill</text></a></td>
                       <td><text style={{ color: 'red' }}>October 17th, 2021 11:59pm EDT</text></td>
                   </tr>
                   <tr>
