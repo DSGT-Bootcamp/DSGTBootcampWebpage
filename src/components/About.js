9f662699ed47634b1bba2d6344a76ce7337da79a
achauhan63 <achauhan63@gatech.edu>
2022-01-23 11:42:55 -0500

    Abhi Onboarding Task.

--- src/components/About.js
+++ src/components/About.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import AboutSlideshow from './AboutSlideshow/index.js';
 class About extends React.Component {
-    render() {
-        return (
-          <div style={{height: "600px", marginBottom: "500px"}}>
-            <div class="container">
-              <div className="home">
-                {/* <div class="col-lg-7">
+  render() {
+    return (
+      <div style={{ height: "600px", marginBottom: "500px" }}>
+        <div class="container">
+          <div className="home">
+            {/* <div class="col-lg-7">
                 </div>
                 <div class="col-lg-5" style={{height: "500px"}}> */}
-                <h1 style={{class: "font-weight-bold", paddingTop: "50px", textAlign: "center", color: "black"}}>About DSGT Bootcamp</h1>
-                  <p style={{alignContent: "center", color: "black"}} class="font-weight-100" >
-                    DSGT bootcamp is an immersive 11-week experience where you’ll
-                    learn a variety of data science skills and work in teams to answer 
-                    significant questions by analyzing data. Our mentors will host 
-                    workshops and guide you through your projects. We work in iterative
-                    milestones and provide feedback on your work. DSGT Bootcamp is the ideal place to 
-                    jumpstart your data science career!
-                  </p>
-                  <a href="/options" style={{color: "blue"}}>Click here to view your options for participating in DSGT Bootcamp</a>
-                  <p style={{color: "black"}}>Hello my name is Abhijat Chauhan and I am a new content creator!</p>
-              </div>
-            </div>
-            <AboutSlideshow/>
-            
+            <h1 style={{ class: "font-weight-bold", paddingTop: "50px", textAlign: "center", color: "black" }}>About DSGT Bootcamp</h1>
+            <p style={{ alignContent: "center", color: "black" }} class="font-weight-100" >
+              DSGT bootcamp is an immersive 11-week experience where you’ll
+              learn a variety of data science skills and work in teams to answer
+              significant questions by analyzing data. Our mentors will host
+              workshops and guide you through your projects. We work in iterative
+              milestones and provide feedback on your work. DSGT Bootcamp is the ideal place to
+              jumpstart your data science career!
+            </p>
+            <a href="/options" style={{ color: "blue" }}>Click here to view your options for participating in DSGT Bootcamp</a>
+            <p style={{ color: "black" }}>Hello my name is Abhijat Chauhan and I am a new content creator!</p>
           </div>
-        );
-    }
+        </div>
+        <AboutSlideshow />
+
+      </div>
+    );
+  }
 }
 
 export default About;
