213b2bf17dd08cb31de974632b9a8f4e5828576f
nzhang321 <89886223+nzhang321@users.noreply.github.com>
2022-01-27 22:16:20 -0500

    Nick1 (#61)
    * added demographic information slideshow on about table
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete ethnicity.png
    
    * Delete gender.png
    
    * Delete location.png
    
    * Delete major.png
    
    * Delete year.png
    
    * Delete index.js
    
    * nick1

--- src/components/BootcampOptions.js
+++ src/components/BootcampOptions.js
@@ -3,19 +3,8 @@ import OptionTable from './OptionTable';
 class BootcampOptions extends React.Component {
     render() {
         return (
-          <div className="home">
-            {/* <div class="container">
-              <div class="row align-items-center my-5">
-                <div class="col-lg-7">
-                </div>
-                <div class="col-lg-5"> */}
+          <div className="body">
                   <h1 style={{class: "font-weight-bold", color:"black", marginTop: "50px", display: "flex", justifyContent: "center"}}>Bootcamp Options</h1>
-                  {/* <p class="font-weight-light" style={{color:"black", display: "flex", justifyContent: "center"}}>
-                    Schedule for DSGT Bootcamp workshops.
-                  </p> */}
-                {/* </div>
-              </div>
-            </div> */}
           <OptionTable/>
           </div>
         );
