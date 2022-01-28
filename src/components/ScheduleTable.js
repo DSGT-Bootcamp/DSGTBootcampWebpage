4dff91037ce352d2217dbef4e90df1deb25023ba
karkir0003 <54720987+karkir0003@users.noreply.github.com>
2022-01-27 22:22:28 -0500

    html comment syntax fix

--- src/components/ScheduleTable.js
+++ src/components/ScheduleTable.js
@@ -20,35 +20,35 @@ class ScheduleTable extends React.Component {
                     <td><text style={{ color: 'black' }}>1</text></td>
                     <td><text style={{ color: 'black' }}>February 7th, 2022 at College of Computing 017</text></td>
                     <td><text style={{ color: 'black' }}>Introduction to Bootcamp</text></td>
-                    <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Workshop 1</NavLink> replace the link with the workshop 1 drive when it is shared
+                    <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Workshop 1</NavLink> <!-- replace the link with the workshop 1 drive when it is shared -->
                     <td><text style={{ color: 'black' }}>February 21st, <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Milestone 1</NavLink></text></td>
                   </tr>
                   <tr>
                     <td><text style={{ color: 'black' }}>2</text></td>
                       <td><text style={{ color: 'black' }}>February 21st, 2022 at College of Computing 017</text></td>
                       <td><text style={{ color: 'black' }}>Exploratory Data Analysis</text></td>
-                      <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Workshop 2</NavLink> //replace the link with the workshop 2 drive when it is shared
+                      <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Workshop 2</NavLink> <!-- replace the link with the workshop 2 drive when it is shared -->
                       <td><text style={{ color: 'black' }}>March 7th, <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Milestone 2</NavLink></text></td>
                   </tr>
                   <tr>
                     <td><text style={{ color: 'black' }}>3</text></td>
                     <td><text style={{ color: 'black' }}>March 7th, 2022 at College of Computing 017</text></td>
                     <td><text style={{ color: 'black' }}>Introductory Data Preprocessing</text></td>
-                    <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Workshop 3</NavLink> //replace the link with the workshop 3 drive when it is shared
+                    <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Workshop 3</NavLink> <!-- replace the link with the workshop 3 drive when it is shared -->
                     <td><text style={{ color: 'black' }}>March 28th, <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Milestone 3</NavLink></text></td>
                   </tr>
                   <tr>
                       <td><text style={{ color: 'black' }}>4</text></td>
                       <td><text style={{ color: 'black' }}>March 28th, 2022 at College of Computing 017</text></td>
                       <td><text style={{ color: 'black' }}>Modeling with Machine Learning</text></td>
-                      <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Workshop 4</NavLink> // replace the link with the workshop 4 drive when it is shared
+                      <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Workshop 4</NavLink> <!-- replace the link with the workshop 4 drive when it is shared -->
                       <td><text style={{ color: 'black' }}>April 11th, <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Milestone 4</NavLink></text></td>
                   </tr>
                   <tr>
                       <td><text style={{ color: 'black' }}>5</text></td>
                       <td><text style={{ color: 'black' }}>April 11th, 2022 at College of Computing 017</text></td>
                       <td><text style={{ color: 'black' }}>Introduction to Deep Learning</text></td>
-                      <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Workshop 5</NavLink> // replace the link with the workshop 5 drive when it is shared
+                      <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Workshop 5</NavLink> <!-- replace the link with the workshop 5 drive when it is shared -->
                       <td><text style={{ color: 'black' }}>April 25th, <NavLink to="/https://www.youtube.com/watch?v=dQw4w9WgXcQ">Milestone 5</NavLink></text></td>
                   </tr>
               </tbody>
