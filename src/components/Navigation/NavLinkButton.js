ca246752e0e2086dd0c1953efa5fabeeba5a85f0
Faris Durrani <afarisdurrani@gmail.com>
2022-02-02 15:03:39 -0500

    Color change and Hover on Navbar

---
+++ src/components/Navigation/NavLinkButton.js
@@ -0,0 +1,54 @@
+import COLORS from "../../constants";
+import { Nav } from "react-bootstrap";
+import { useState } from "react";
+
+const NavLinkButton = (props) => {
+  const [hovering, setHovering] = useState(false);
+
+  // src: https://stackoverflow.com/a/33000431/11031425
+  if (hovering) {
+    styles.navBar_link_current = styles.navBar_link_hover;
+  } else {
+    styles.navBar_link_current = styles.navBar_link_normal;
+  }
+
+  return (
+    <Nav.Link
+      href={props.url_extension}
+      style={styles.navBar_link_current}
+      onMouseEnter={() => {
+        setHovering(true);
+      }}
+      onMouseLeave={() => {
+        setHovering(false);
+      }}
+    >
+      {props.children}
+    </Nav.Link>
+  );
+};
+
+const styles = {
+  navBar: {
+    background: COLORS["primary_blue"],
+    width: "100%",
+    height: "80px",
+    position: "sticky",
+  },
+  container: {
+    display: "flex",
+    alignItems: "center",
+    lineHeight: "50px",
+  },
+  navBar_link_current: {
+    color: "white",
+  },
+  navBar_link_normal: {
+    color: "white",
+  },
+  navBar_link_hover: {
+    color: "#FFFFFF80",
+  },
+};
+
+export default NavLinkButton;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,49 +1,76 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import Logo from '../../images/dsgtlogo (1).png';
+import { useState } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import Logo from "../../images/dsgtlogo (1).png";
+import COLORS from "../../constants";
+import NavLinkButton from "./NavLinkButton";
 
 const Navigation = () => {
-    return (
-        <div style={{background: "#011c33", width: "100%", height: "100px", position: "sticky"}}>
-            <Navbar collapseOnSelect expand='sm' variant='dark' class='container-fluid'>
-                <Container>
-                    <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
-                    <Navbar.Collapse id='responsive-navbar-nav' style={{display: "flex", alignItems: "center", lineHeight: "60px"}}>
-                        <Nav>
-                        <Nav.Item className="ml-auto">
-                        <img
-                            src = {Logo}
-                            alt="dsgt logo"
-                            width="32"
-                            height="32"
-                            display="flex"
-                            lineHeight="60px"
-                        />                        
-                        </Nav.Item>
-                            <Nav.Item className="ml-auto">
-                                <Nav.Link href='/'>Home</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item className="ml-auto">
-                                <Nav.Link href='/about'>About</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item className="ml-auto">
-                                <Nav.Link href='/schedule'>Schedule</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item className="ml-auto">
-                                <Nav.Link href='/mentors'>Mentors</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item className="ml-auto">
-                                <Nav.Link href='/resources'>Resources</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item className="ml-auto">
-                                <Nav.Link href='/options'>Bootcamp Options</Nav.Link>
+  return (
+    <div style={styles.navBar}>
+      <Navbar
+        collapseOnSelect
+        expand="sm"
+        variant="dark"
+        class="container-fluid"
+      >
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" style={styles.container}>
+            <Nav>
+              <Nav.Item className="ml-auto">
+                <img
+                  src={Logo}
+                  alt="dsgt logo"
+                  width="69"
+                  height="69"
+                  display="flex"
+                  lineheight="69"
+                />
+              </Nav.Item>
+              <Nav.Item className="ml-auto">
+                <NavLinkButton url_extension="/">Home</NavLinkButton>
+              </Nav.Item>
+              <Nav.Item className="ml-auto">
+                <NavLinkButton url_extension="/about">About</NavLinkButton>
+              </Nav.Item>
+              <Nav.Item className="ml-auto">
+                <NavLinkButton url_extension="/schedule">
+                  Schedule
+                </NavLinkButton>
+              </Nav.Item>
+              <Nav.Item className="ml-auto">
+                <NavLinkButton url_extension="/mentors">Mentors</NavLinkButton>
+              </Nav.Item>
+              <Nav.Item className="ml-auto">
+                <NavLinkButton url_extension="/resources">
+                  Resources
+                </NavLinkButton>
+              </Nav.Item>
+              <Nav.Item className="ml-auto">
+                <NavLinkButton url_extension="/options">
+                  Bootcamp Options
+                </NavLinkButton>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
 
-                            </Nav.Item>  
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
-    );
-}
+const styles = {
+  navBar: {
+    background: COLORS["primary_blue"],
+    width: "100%",
+    height: "80px",
+    position: "sticky",
+  },
+  container: {
+    display: "flex",
+    alignItems: "center",
+    lineHeight: "50px",
+  },
+};
 
 export default Navigation;
