9f23bdc1abd479ff718b2c85d4b317c3706a7e98
Faris Durrani <afarisdurrani@gmail.com>
2022-02-05 23:56:48 -0500

    CI test fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders dsgt bootcamp link', () => {
+test("renders dsgt bootcamp link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/DSGT Bootcamp/i);
+  const linkElement = screen.getByText(
+    /Data Science at Georgia Tech Bootcamp/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
