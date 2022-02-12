b7f8f8e13e17947bea7de606a79bb5b8d51adc2c
Faris Durrani <afarisdurrani@gmail.com>
2022-02-12 11:54:47 -0500

    Mentors page v1 complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
-import Home from './components/HomeScreen/Home.js';
 import About from './components/About.js';
-import Schedule from './components/Schedule/Schedule';
-import Mentors from './components/Mentors';
-import Resource from './components/Resource';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import Contact from './components/Contact/index';
-import ReactGA from 'react-ga';
-import RouteChangeTracker from './components/RouteChangeTracker';
 import BootcampOptions from './components/BootcampOptions';
+import Contact from './components/Contact/index';
+import Home from './components/HomeScreen/Home.js';
+import Mentors from './components/Mentors/index';
+import Navigation from './components/Navigation';
+import ReactGA from 'react-ga';
+import Resource from './components/Resource';
+import RouteChangeTracker from './components/RouteChangeTracker';
+import Schedule from './components/Schedule/index';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const TRACKING_ID = "UA-185881457-1"; // YOUR_OWN_TRACKING_ID
 ReactGA.initialize(TRACKING_ID, { testMode: process.env.NODE_ENV === 'test' });
---
+++ src/components/Mentors/Mentor.js
@@ -0,0 +1,73 @@
+import COLORS from "../../constants";
+import React from "react";
+import { Row, Image, Container } from "react-bootstrap";
+
+const Mentor = (props) => {
+  const [name, img, headline, fun_fact] = [
+    props.name,
+    props.img,
+    props.headline,
+    props.fun_fact,
+  ];
+
+  return (
+    <Row style={styles.mainContainer}>
+      <Image
+        style={styles.pic}
+        src={img}
+        alt={name + "'s picture"}
+        roundedCircle
+      />
+      <Container style={styles.detailsContainer}>
+        <h2 style={styles.title}>{name}</h2>
+        <hr style={styles.line} />
+        <p style={styles.headline}>{headline}</p>
+        <p style={styles.fun_fact}>“{fun_fact}”</p>
+      </Container>
+    </Row>
+  );
+};
+
+const styles = {
+  mainContainer: {
+    paddingTop: "20px",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+  },
+  title: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: "3em",
+    color: COLORS.primary_blue,
+  },
+  pic: {
+    maxWidth: "250px",
+    maxHeight: "250px",
+  },
+  title: {
+    fontSize: "2em",
+    fontWeight: "650",
+    color: COLORS.primary_blue,
+  },
+  detailsContainer: {
+    maxWidth: "60%",
+    margin: "0 0 0 30px",
+  },
+  headline: {
+    color: COLORS.light_blue,
+  },
+  line: {
+    borderColor: COLORS.primary_green,
+    borderWidth: "3px",
+    margin: "0",
+    width: "50%",
+  },
+  fun_fact: {
+    color: COLORS.primary_blue,
+    fontSize: "1.5em",
+    fontStyle: "italic",
+  },
+};
+
+export default Mentor;
