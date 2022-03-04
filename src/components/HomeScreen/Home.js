b0e244cfdbb9c39db45f68beb63f290144b2fe92
Faris Durrani <afarisdurrani@gmail.com>
2022-03-04 14:39:13 -0500

    Removed Welcome to New Design Popup (#77)
    * onboarding commit TODO revert changes
    
    * reverted onboarding commit
    
    * update from main
    
    * included How to Run on Readme
    
    * test commit
    
    * undo last commit
    
    * Prettier style update and className fixes
    
    * Color change and Hover on Navbar
    
    * Reordered NavBar, added NavBar.Brand
    
    * Fixed NavBar stickiness and Brand
    
    * NavBar v1 Complete --  to fix half screen view
    
    * First homepage screen established --logo issue
    
    * FirstScreenScroll v1 done
    
    * SecondScrollScreen v1 done
    
    * Minor: Button hovering function created
    
    * ThirdScrollScreen v1 done
    
    * Established FourthScrollScreen --still a lot to do
    
    * Completed HomeScreen v1
    
    * CI test fix
    
    * trying another CI fix
    
    * another CI fix attempt
    
    * footer v1 completed
    
    * Schedule page v1 complete --missing links
    
    * Minor: deleted unnecessary files
    
    * Minor: updated links on Schedule
    
    * Mentors page v1 complete
    
    * Resources page v1 complete
    
    * Footer and Resources fixes
    
    * deleted duplicate photoshoots
    
    * bootcamp options v1 completed
    
    * Minor: code cleanups
    
    * General code cleanup
    
    * Code damage fixes
    
    * About pie charts v1 complete
    
    * About Gender v1 complete
    
    * About page v1 complete
    
    * Minor: added titles for past projects
    
    * General Code Refurbish
    
    * Code cleanup + add tooltips for pie charts
    
    * Message box v1 complete
    
    * Minor: pie chart color update
    
    * Update to incorrect text values
    
    * More corrections
    
    * Added Kruti's picture --all done
    
    * Fixed Netlify issues
    
    * Fixed alt netlify
    
    * Minor: NavBar Toggle Button made light
    
    * Removed New Design Popup

--- src/components/HomeScreen/Home.js
+++ src/components/HomeScreen/Home.js
@@ -3,22 +3,14 @@ import FourthScrollScreen from "./FourthScrollScreen";
 import React from "react";
 import SecondScrollScreen from "./SecondScrollScreen";
 import ThirdScrollScreen from "./ThirdScrollScreen";
-import WelcomeToNewMsg from "./WelcomeToNewMsg";
 
 const Home = () => {
-  const [open, setOpen] = React.useState(true);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
     <div id="homeScreenMainDiv">
       <FirstScrollScreen />
       <SecondScrollScreen />
       <ThirdScrollScreen />
       <FourthScrollScreen />
-      <WelcomeToNewMsg open={open} handleClose={handleClose} />
     </div>
   );
 };
